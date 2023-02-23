5956b35a5302ef69cd396c158f5fe2b87a9d0ad3
Meeri <meeri.simpanen@helsinki.fi>
2023-02-23 16:40:58 +0100

    fixing comment

--- comments.py
+++ comments.py
@@ -3,7 +3,7 @@ from flask import session
 from datetime import datetime
 
 def add_comment(comment, user_id, story_id):
-    sql = """INSERT INTO messages (comment, user_id, story_id)
+    sql = """INSERT INTO comments (comment, user_id, story_id)
             VALUES (:comment, :user_id, :story_id"""
     db.session.execute(sql, {"comment":comment, "user_id":user_id, "story_id:":story_id})
     db.session.commit()
